ad1a405541513352c934e59cd8abaea373ba567b
nastasyas0 <nastya.strelkova.03.00@mail.ru>
2022-10-12 17:31:08 +0300

    button styles

---
+++ app/src/main/java/com/example/quiz/GameLevels.java
@@ -0,0 +1,65 @@
+package com.example.quiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameLevels extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gamelevels);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Button button_back = (Button)findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Команда для кнопки "Назад"
+                try{
+                    Intent intent = new Intent(GameLevels.this, MainActivity.class);
+                    startActivity(intent);finish();
+                }catch (Exception e) {
+
+                }
+            }
+        });
+
+        // Кнопка для перехода на 1 уровень викторины - начало
+        TextView textView1 = (TextView)findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    startActivity(intent);finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 1 уровень викторины - конец
+    }
+    // Системная кнопка "Назад" - начало
+    // Возвращение на главный экран по нажатию системной кнопки "Назад"
+    @Override
+    public void onBackPressed() {
+        try{
+            Intent intent = new Intent(GameLevels.this, MainActivity.class);
+            startActivity(intent);finish();
+        }catch (Exception e) {
+
+        }
+    }
+    // Системная кнопка "Назад" - конец
+}
