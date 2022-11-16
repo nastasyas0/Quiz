af010f1e8f3bc9b4e3a79bd519138d9acb4a7c23
Ангелина <lina.shalimanova@list.ru>
2022-11-16 11:15:17 +0300

    all_buttons_with_questions

--- app/src/main/java/com/example/quiz/GameLevels.java
+++ app/src/main/java/com/example/quiz/GameLevels.java
@@ -41,14 +41,281 @@ public class GameLevels extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
+                    String name = "rus_10.txt";//параметр для нового activity
                     Intent intent = new Intent(GameLevels.this, Level1.class);
-                    startActivity(intent);finish();
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
                 } catch(Exception e) {
 
                 }
             }
         });
-        // Кнопка для перехода на 1 уровень викторины - конец
+        // Кнопка для перехода на 2 уровень викторины - конец
+        // Кнопка для перехода на 2 уровень викторины - начало
+        TextView textView2 = (TextView)findViewById(R.id.textView2);
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "rus_20.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 2 уровень викторины - конец
+        // Кнопка для перехода на 3 уровень викторины - начало
+        TextView textView3 = (TextView)findViewById(R.id.textView3);
+        textView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "rus_30.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 3 уровень викторины - конец
+        // Кнопка для перехода на 4 уровень викторины - начало
+        TextView textView4 = (TextView)findViewById(R.id.textView4);
+        textView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "rus_40.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 4 уровень викторины - конец
+
+        //новый предмет математика
+        // Кнопка для перехода на 5 уровень викторины - начало
+        TextView textView5 = (TextView)findViewById(R.id.textView5);
+        textView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "math_10.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 5 уровень викторины - конец
+
+        // Кнопка для перехода на 6 уровень викторины - начало
+        TextView textView6 = (TextView)findViewById(R.id.textView6);
+        textView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "math_20.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 6 уровень викторины - конец
+
+        // Кнопка для перехода на 7 уровень викторины - начало
+        TextView textView7 = (TextView)findViewById(R.id.textView7);
+        textView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "math_30.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 7 уровень викторины - конец
+
+        // Кнопка для перехода на 8 уровень викторины - начало
+        TextView textView8 = (TextView)findViewById(R.id.textView8);
+        textView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "math_40.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 8 уровень викторины - конец
+        //новый предмет информатика
+
+        // Кнопка для перехода на 9 уровень викторины - начало
+        TextView textView9 = (TextView)findViewById(R.id.textView9);
+        textView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "inf_10.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 9 уровень викторины - конец
+        // Кнопка для перехода на 10 уровень викторины - начало
+        TextView textView10 = (TextView)findViewById(R.id.textView10);
+        textView10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "inf_20.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 10 уровень викторины - конец
+        // Кнопка для перехода на 11 уровень викторины - начало
+        TextView textView11 = (TextView)findViewById(R.id.textView11);
+        textView11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "inf_30.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 11 уровень викторины - конец
+        // Кнопка для перехода на 12 уровень викторины - начало
+        TextView textView12 = (TextView)findViewById(R.id.textView12);
+        textView12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "inf_40.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 12 уровень викторины - конец
+        //новый предмет английский
+        // Кнопка для перехода на 13 уровень викторины - начало
+        TextView textView13 = (TextView)findViewById(R.id.textView13);
+        textView13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "eng_10.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 13 уровень викторины - конец
+        // Кнопка для перехода на 14 уровень викторины - начало
+        TextView textView14 = (TextView)findViewById(R.id.textView14);
+        textView14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "eng_20.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 14 уровень викторины - конец
+
+        // Кнопка для перехода на 15 уровень викторины - начало
+        TextView textView15 = (TextView)findViewById(R.id.textView15);
+        textView15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "eng_30.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 15 уровень викторины - конец
+        // Кнопка для перехода на 16 уровень викторины - начало
+        TextView textView16 = (TextView)findViewById(R.id.textView16);
+        textView16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String name = "eng_40.txt";//параметр для нового activity
+                    Intent intent = new Intent(GameLevels.this, Level1.class);
+                    intent.putExtra("name",name);//передаем нужный параметр
+                    startActivity(intent);
+                    finish();
+                } catch(Exception e) {
+
+                }
+            }
+        });
+        // Кнопка для перехода на 16 уровень викторины - конец
     }
     // Системная кнопка "Назад" - начало
     // Возвращение на главный экран по нажатию системной кнопки "Назад"
--- app/src/main/java/com/example/quiz/Level1.java
+++ app/src/main/java/com/example/quiz/Level1.java
@@ -2,10 +2,12 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,38 +28,70 @@ public class Level1 extends AppCompatActivity {
         setContentView(R.layout.universal);
 
         Window w = getWindow();
-        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        //кнопка перехода к списку вопросов
+        Button button_back = (Button)findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Команда для кнопки "Назад"
+                try{
+                    Intent intent = new Intent(Level1.this, GameLevels.class);
+                    startActivity(intent);finish();
+                }catch (Exception e) {
+
+                }
+            }
+        });
+
+
 
         questionField = findViewById(R.id.question);//поле для вопроса
 
-        //View v;
+        putQuestionInFile();
         getQuestionFromFile();
-        //questionField.setText("Hello");
     }
-
-    //берём вопрос из файла
-    public void getQuestionFromFile()  {
+    //Системная кнопка "Назад" - список вопросов
+    // Возвращение на экран с вопросами по нажатию системной кнопки "Назад"
+    @Override
+    public void onBackPressed() {
         try{
-            FileOutputStream f = openFileOutput("rus_10.txt", MODE_PRIVATE);//заполнение файла
-            String d="Hello, world";
-            f.write(d.getBytes());
+            Intent intent = new Intent(Level1.this, GameLevels.class);
+            startActivity(intent);finish();
+        }catch (Exception e) {
+
         }
-        catch (FileNotFoundException e){
+    }
+    public void putQuestionInFile(){
+        Intent intent = getIntent();//переданный параметр из другой activity(название файла)
+        try {
+            String name = intent.getStringExtra("name");//полученные данные
+            FileOutputStream f = openFileOutput(name, MODE_PRIVATE);//заполнение файла
+            String d = name;
+            f.write(d.getBytes());
+            f.close();
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-            Toast.makeText(this,"Не получается обработать файл.", Toast.LENGTH_SHORT);
+            Toast.makeText(this, "Не получается обработать файл.", Toast.LENGTH_SHORT);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    //берём вопрос из файла
+    public void getQuestionFromFile() {
+        ///data/data/com.example.quiz/files/eng_10.txt
+        Intent intent = getIntent();//переданный параметр из другой activity(название файла)
+        String name = intent.getStringExtra("name");//полученные данные
         //чтение из файла
-        String question;
-        try{
-            FileInputStream fileInput = openFileInput("rus_10.txt");//с каким файлом работаем
+        try {
+            FileInputStream fileInput = openFileInput(name);//с каким файлом работаем
             InputStreamReader reader = new InputStreamReader(fileInput);//считали данные
             BufferedReader bufferedReader = new BufferedReader(reader);//преобразовали в нужный вид
             questionField.setText(bufferedReader.readLine());
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
